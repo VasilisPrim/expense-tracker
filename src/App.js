080d1fb7be27e00f347d1da1d8fdfd668ffb92a4
Bill <bprimikiris@gmail.com>
2022-12-16 18:35:44 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Todo from "./components/Todo";
+
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="First comp" />
+      <Todo text="Second comp" />
+      
+    </div>
+  );
+}
+
+export default App;
